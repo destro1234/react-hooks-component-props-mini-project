64b441f14a291df51c53233b8294974c2c90a040
Elias <eliasventura23@gmail.com>
2022-07-05 21:28:54 -0400

    commit tree b1db5cf534eec7544285b01fa8a7e3da60af35a9

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,19 @@
 import React from "react";
 import blogData from "../data/blog";
+import Header from './Header.js'
+import About from './About.js'
+import logo from "../assets/logo.svg"
+import ArticleList from './ArticleList.js'
 
-console.log(blogData);
+
 
 function App() {
+  console.log(blogData.posts)
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+      <Header name="Underreacted"/>
+      <About src= {logo} about={blogData.about}/>
+      <ArticleList posts={blogData.posts} />
     </div>
   );
 }
